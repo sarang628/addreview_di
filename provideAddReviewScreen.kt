66b4d9b2c34fcbf1c052411751ebde81deb527f3
sryang <sarang628@gmail.com>
2026-02-10 16:53:19 +0900

    fix after uploaded move to main

--- provideAddReviewScreen.kt
+++ provideAddReviewScreen.kt
@@ -1,7 +1,10 @@
 package com.sarang.torang.di.addreview_di
 
 import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.rememberNavController
 import com.example.instagramgallery.di.Instagramgallery_di.GalleryWithPhotoPicker
@@ -10,19 +13,23 @@ import com.sarang.torang.addreview.compose.AddReviewScreen
 import com.sarang.torang.addreview.compose.Login
 
 @OptIn(ExperimentalMaterial3Api::class)
-fun provideAddReviewScreen(navHostController: RootNavController = RootNavController()): @Composable (onClose: () -> Unit) -> Unit =
+fun provideAddReviewScreen(navHostController : RootNavController = RootNavController()
+): @Composable (onClose: () -> Unit) -> Unit =
     { onCloseReview ->
         val navController = rememberNavController()
         val dispatch = LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher
+        Box {
             AddReviewScreen(
                 galleryScreen = { GalleryWithPhotoPicker(onNext = it.onNext, onClose = { it.onClose.invoke() }) },
                 navController = navController,
                 onRestaurant = { navController.navigate("addReview") },
-                onShared = { navHostController.popBackStack() },
+                onShared = { onCloseReview()
+                             navController.popBackStack("gallery", false) },
                 onNext = { navController.navigate("selectRestaurant") },
-                onClose = { onCloseReview.invoke() },
+                onClose = { onCloseReview() },
                 onNotSelected = { navController.navigate("addReview") },
                 loginScreen = { Login(onLogin = { navHostController.emailLogin() },
-                                      onBack = { dispatch?.onBackPressed() }) }
+                    onBack = { dispatch?.onBackPressed() }) }
             )
+        }
     }
